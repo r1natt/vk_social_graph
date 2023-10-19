a9f446798e33f0d9957706a21c8f2c68f5f85c55
delamcon <rinatt116@gmail.com>
2023-10-19 14:51:56 +0300

    User data is obj now

--- user_data.py
+++ user_data.py
@@ -1,18 +1,28 @@
 import json
+from datetime import datetime
 
 
-class UserData():
+class User(dict):
+    def __init__(self, user_data):
+        self.update(user_data)
+        self.change_id()
+
+    def change_id(self):
+        _id = self["id"]
+        del self["id"]
+        self["_id"] = _id
+
+
+class Old:
     def __init__(self, owner_id, user_data):
         self.user_data = user_data
         self.owner_id = owner_id
         self._cook_user_data()
 
     def __repr__(self):
-        print(self.user_data)
         return json.dumps(self.user_data, ensure_ascii=False)
 
     def _cook_user_data(self):
-        self._user_handler()
         self._user_add_date()
     
     def _user_add_date(self):
@@ -26,8 +36,3 @@ class UserData():
         updated_data[today] = self.user_data
         self.user_data = updated_data
 
-    def _user_handler(self):
-        self.user_data["_id"] = self.user_data["id"]
-        del self.user_data["id"]
-        self.user_data["first_name"] = self.user_data["first_name"].replace("/", "-").replace("_", "-")
-        self.user_data["last_name"] = self.user_data["last_name"].replace("/", "-").replace("_", "-")
