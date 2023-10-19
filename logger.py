72754f31f38b4632aa5ffe74f3f6c66df5446e4c
delamcon <rinatt116@gmail.com>
2023-10-19 14:52:46 +0300

    add colorlog

--- logger.py
+++ logger.py
@@ -1,13 +1,21 @@
 import logging
+from colorlog import ColoredFormatter
 
 
-formatter = logging.Formatter(fmt=None, datefmt=None, style='%')
+STREAM_LOG_LEVEL = logging.DEBUG
+FILE_LOG_LEVEL = logging.INFO
+LOGFORMAT = "%(log_color)s%(asctime)s %(name)s [%(levelname)s] - %(message)s (%(filename)s:%(lineno)s) %(reset)s"
+DATEFORMAT = "%d-%m-%Y %H:%M:%s"
+formatter = ColoredFormatter(LOGFORMAT)
+
 stream = logging.StreamHandler()
 stream.setLevel(logging.DEBUG)
 stream.setFormatter(formatter)
+
 fh = logging.FileHandler("./logger.log")
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
+
 logger = logging.getLogger("logger")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(fh)
