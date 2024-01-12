c0c15cdfaa909c165f8b73f9c59fc0927989fbfa
delamcon <rinatt116@gmail.com>
2024-01-12 12:10:36 +0300

    add config file example

---
+++ config.py
@@ -0,0 +1,3 @@
+token = "..."  # Токен приложения ВК для работы парсера
+               # Получить токен можно здесь: https://dev.vk.com/ru/api/access-token/implicit-flow-community
+mongo_uri = "mongodb://localhost:27017/"  # Адрес для удаленного подключения к mongodb
