b43ac0c12d3a99025e744d846f3ff4c47aae075c
delamcon <rinatt116@gmail.com>
2024-01-13 22:26:33 +0300

    change log level to INFO, delete colorlog from format string

--- logger.py
+++ logger.py
@@ -4,12 +4,12 @@ from colorlog import ColoredFormatter
 
 STREAM_LOG_LEVEL = logging.DEBUG
 FILE_LOG_LEVEL = logging.INFO
-LOGFORMAT = "%(log_color)s%(asctime)s %(name)s [%(levelname)s] - %(message)s (%(filename)s:%(lineno)s) %(reset)s"
+LOGFORMAT = "%(asctime)s %(name)s [%(levelname)s] - %(message)s (%(filename)s:%(lineno)s)"
 DATEFORMAT = "%d-%m-%Y %H:%M:%s"
 formatter = ColoredFormatter(LOGFORMAT)
 
 stream = logging.StreamHandler()
-stream.setLevel(logging.DEBUG)
+stream.setLevel(logging.INFO)
 stream.setFormatter(formatter)
 
 fh = logging.FileHandler("./logger.log")
@@ -19,4 +19,5 @@ fh.setFormatter(formatter)
 logger = logging.getLogger("logger")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(fh)
-logger.addHandler(stream)
+logger.addHandler(stream)  # Откоментирование этой строки добавляет вывод 
+                             # логов в терминал
