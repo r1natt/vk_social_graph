f924d346f8ad0d16468aec43c9328a2e6109ae09
delamcon <rinatt116@gmail.com>
2024-01-14 01:36:07 +0300

    recreate errors class, delete error_check()

--- errors.py
+++ errors.py
@@ -1,47 +1,40 @@
-from logger import logger
+from logger import reqs_logger 
 
 
-class ErrorsCheck:
-    def __init__(self):
-        """
-        {"error":
-            {"error_code":5,
-            "error_msg":"User authorization failed: access_token has expired.",
-            "request_params":[
-                {"key":"user_id","value":"3...6"},
-                ...,
-                {"key":"v","value":"5.154"}
-                ]
-            }
-        }
-        Выше пример запроса с ошибкой
-        """
-        pass
+class ExpiredToken(Exception):
+    pass
 
-def _get_vk_id_in_response(response):
-    # данная функция находит user_id который был отправлен в запросе в 
-    # ответе, который представлен в __init__
-    params = response["error"]["requests_params"]
-    for param_dict in params:
-        if param_dict["key"] == "user_id":
-            return param_dict["value"]
-    return None
 
-def error_check(vk_api_response) -> bool | int:
-    if 'error' in vk_api_response:
-        code = vk_api_response["error"]["error_code"]
-        
-        match code:
-            case 30:
-                response = {"id": _get_vk_id_in_response(response),
-                            "is_closed": True}
-                return 30
+class Errors:
+    def __init__(self, response: dict):
+        self.response = response
+        self.is_error = False
+        self.code = -1
+
+        self.check()
+
+    def _get_vk_id_from_error_response(self):
+        params = self.response["error"]["request_params"]
+        for param_dict in params:
+            if param_dict["key"] == "user_id":
+                return param_dict["value"]
+        return None
+
+    def _is_error_in_response(self):
+        if 'error' in self.response:
+            self.is_error = True
+            self.code = self.response["error"]["error_code"]
+        return self.is_error
+
+    def check(self):
+        if self._is_error_in_response():
+            self.match_codes()
+        return 
+
+    def match_codes(self):
+        match self.code:
             case 5:
-                logger.error("Update auth key")
-                return 5
-            case 18:
-                return 18
-            case _:
-                print("Error", vk_api_response)
-                return -1
-    return 0  # если нет ошибки, возвращает 0, в ином случае номер ошибки
+                raise ExpiredToken("Update access token")
+            case 30:
+                _id = self._get_vk_id_from_error_response()
+                reqs_logger.debug(f"profile {_id} is private")
