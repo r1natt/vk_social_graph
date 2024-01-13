17de123b78277758c20015e7506ab06172e121b0
delamcon <rinatt116@gmail.com>
2024-01-14 01:34:53 +0300

    add TPS_BUCKET

--- reqs.py
+++ reqs.py
@@ -1,11 +1,76 @@
 import requests
 import json
 from config import token
-from logger import logger
+from logger import reqs_logger
+from multiprocessing import Process, Value
+import datetime
+from errors import Errors
+import time
+
+"""
+TPS - transaction per second
+VK API имеют лимиты на запросы (5 запросов в секунду, но позволяют они немного 
+больше). Все дело в том, что пока я выполняю запросы к api без прокси и в 
+одном процессе, поэтому мне нужно, чтобы запросы проходили в одной очереди с 
+минимальными задержками, пока не достигнут лимит запросов в секунду
+
+TPSBucket - решает эту проблему, по сути своей это отдельный процесс, который 
+следит за достижением лимита, и каждую секунду обновляет количество запросов 
+на заданное число
+
+Важно отметить, что этот класс будет работать и с запросами, поступающеми от 
+разных процессов одновременно
+"""
+class TPSBucket:
+    def __init__(self, expected_tps):
+        self.number_of_tokens = Value('i', 0)
+        self.expected_tps = expected_tps
+        self.bucket_refresh_process = Process(target=self.refill_bucket_per_second)
+
+    def refill_bucket_per_second(self):
+        while True:
+            self.refill_bucket()
+            time.sleep(1)
+
+    def refill_bucket(self):
+        self.number_of_tokens.value = self.expected_tps
+
+    def start(self):
+        self.bucket_refresh_process.start()
+
+    def stop(self):
+        self.bucket_refresh_process.kill()
+
+    def get_token(self):
+        response = False
+        if self.number_of_tokens.value > 0:
+            with self.number_of_tokens.get_lock():
+                if self.number_of_tokens.value > 0:
+                    self.number_of_tokens.value -= 1
+                    response = True
+        return response
 
 
+"""
+Декоратор, который проверяет достигнут ли лимит запросов в эту секунду, если 
+он достигнут, цикл while будет ждать следующей секунды, чтобы выполнить 
+заданную функцию
+"""
+def bucket_queue(func):
+    def exec_or_wait(*args, **kwargs):
+        while True:
+            if tps_bucket.get_token():
+                return func(*args, **kwargs)
+    return exec_or_wait
 
-def get_friends(vk_id, advanced=True):
+
+tps_bucket = TPSBucket(expected_tps=5)
+
+
+@bucket_queue
+def get_friends(vk_id):
+    reqs_logger.debug(f"Friends request: {vk_id}")
+    
     response = requests.get("https://api.vk.com/method/friends.get",
         params={"user_id": vk_id,
                 "access_token": token,
@@ -14,12 +79,14 @@ def get_friends(vk_id, advanced=True):
                 "count": 1000
             }
         )
-    
-    logger.debug(f"Friends request: {vk_id}")
-
     json_response = json.loads(response.text)
+    
+    if Errors(json_response).is_error:
+        json_response = []
+
     return json_response
 
+@bucket_queue
 def get_users_info(user_ids: list):
     """
     Функция запрашивает данные сразу о нескольких пользователях c целью
@@ -51,3 +118,4 @@ def get_users_info(user_ids: list):
         )
     json_response = json.loads(response.text)
     return json_response
+
