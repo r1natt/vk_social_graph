6967d1696dfe974a21d87063a07dc13cf0fe1f91
lewis <rinatt116@gmail.com>
2025-07-16 22:48:51 +0300

    Fully refactored reqs

--- utils/reqs.py
+++ utils/reqs.py
@@ -3,73 +3,211 @@ import time
 from collections import deque
 from functools import wraps
 import json
-from typing import Optional, Callable
+from typing import Union, Optional, Callable
+from enum import Enum, IntEnum
+from dataclasses import dataclass
 from abc import ABC, abstractclassmethod
 
 import requests
-from config import API_KEY
-from models._types import FriendsResponse, UserInfo
+from requests.exceptions import ConnectionError, Timeout
+from config.config import API_KEY
+from models._types import FriendsModel, UserModel
 from .errors import VKApiErrorHandler, ErrorHandlerResult, ErrorAction
+
 
 logger = logging.getLogger(__name__)
 VK_API_VERSION = 5.199
 ERROR_RESPONSE = {"count": 0, "items": []}
 
+class HTTPMethods(Enum):
+    GET = "get"
+    POST = "post"
 
-class RetryPolicy:
-    def __init__(self, max_retries=3):
-        self.vk_error_handler = VKApiErrorHandler()
+class VKAPIResource(Enum):
+    FRIENDS = "friends.get"
+    USERS = "users.get"
+    EXECUTE = "execute"  # Для кода на языке VKScript
 
-        self.max_retries = max_retries
-        self.retry_exceptions = (
-            requests.exceptions.ConnectionError,
-            requests.exceptions.Timeout
-        )
+class VKErrorCode(IntEnum):
+    UNKNOWN_ERROR = 1
+    APP_DISABLED = 2
+    METHOD_NOT_FOUND = 3
+    INVALID_SIGNATURE = 4
+    AUTH_FAILED = 5
+    TOO_MANY_REQUESTS = 6
+    PERMISSION_DENIED = 7
+    BAD_REQUEST = 8
+    SPAM_LIMIT = 9
+    INTERNAL_ERROR = 10
+    CAPTCHA_NEEDED = 14
+    ACCESS_DENIED = 15
+    HTTPS_REQUIRED = 16
+    VALIDATION_REQUIRED = 17
+    PAGE_BLOCKED = 18
+    NON_STANDALONE_FORBIDDEN = 20
+    STANDALONE_ONLY = 21
+    METHOD_DISABLED = 23
+    USER_CONFIRM_REQUIRED = 24
+    COMMUNITY_TOKEN_INVALID = 27
+    APP_TOKEN_INVALID = 28
+    METHOD_RATE_LIMIT = 29
+    PROFILE_PRIVATE = 30
+    PARAM_MISSING_OR_INVALID = 100
+    INVALID_API_ID = 101
+    INVALID_USER_ID = 113
+    INVALID_TIMESTAMP = 150
+    ALBUM_ACCESS_DENIED = 200
+    AUDIO_ACCESS_DENIED = 201
+    GROUP_ACCESS_DENIED = 203
+    ALBUM_FULL = 300
+    PAYMENTS_DISABLED = 500
+    ADS_NO_ACCESS = 600
+    ADS_ERROR = 603
+
+@dataclass
+class ErrorSeverity:
+    NON_CRITICAL = "non_critical"
+    RETRYABLE = "retryable"
+    CRITICAL = "critical"
+    UNKNOWN = "unknown"
+
+@dataclass
+class APIError:
+    code: int
+    message: str
+    severity: ErrorSeverity
+    is_retryable: bool = False
+
+@dataclass
+class APIResponse:
+    success: bool
+    data: Optional[dict[str, Union[dict, str, int]]]
+    error: Optional[APIError]
+
+class ErrorClassifier(ABC):
+    @abstractclassmethod
+    def classify_error(self, code: int, message: str) -> ErrorSeverity:
+        pass
+
+    @abstractclassmethod
+    def is_retryable(self, code: int) -> bool:
+        pass
+
+class VKAPIErrorClassifier(ErrorClassifier):
+    NON_CRITICAL = {
+        VKErrorCode.PERMISSION_DENIED,
+        VKErrorCode.ACCESS_DENIED,
+        VKErrorCode.PAGE_BLOCKED,
+        VKErrorCode.PROFILE_PRIVATE,
+        VKErrorCode.ALBUM_ACCESS_DENIED,
+        VKErrorCode.AUDIO_ACCESS_DENIED,
+        VKErrorCode.GROUP_ACCESS_DENIED
+    }
+
+    RETRYABLE = {
+        VKErrorCode.TOO_MANY_REQUESTS,
+        VKErrorCode.SPAM_LIMIT,
+        VKErrorCode.INTERNAL_ERROR,
+        VKErrorCode.CAPTCHA_NEEDED,
+        VKErrorCode.METHOD_RATE_LIMIT
+    }
+
+    CRITICAL = {
+        VKErrorCode.UNKNOWN_ERROR,
+        VKErrorCode.APP_DISABLED,
+        VKErrorCode.METHOD_NOT_FOUND,
+        VKErrorCode.INVALID_SIGNATURE,
+        VKErrorCode.AUTH_FAILED,
+        VKErrorCode.BAD_REQUEST,
+        VKErrorCode.HTTPS_REQUIRED,
+        VKErrorCode.VALIDATION_REQUIRED,
+        VKErrorCode.NON_STANDALONE_FORBIDDEN,
+        VKErrorCode.STANDALONE_ONLY,
+        VKErrorCode.METHOD_DISABLED,
+        VKErrorCode.USER_CONFIRM_REQUIRED,
+        VKErrorCode.COMMUNITY_TOKEN_INVALID,
+        VKErrorCode.APP_TOKEN_INVALID,
+        VKErrorCode.PARAM_MISSING_OR_INVALID,
+        VKErrorCode.INVALID_API_ID,
+        VKErrorCode.INVALID_USER_ID,
+        VKErrorCode.INVALID_TIMESTAMP,
+        VKErrorCode.ALBUM_FULL,
+        VKErrorCode.PAYMENTS_DISABLED,
+        VKErrorCode.ADS_NO_ACCESS,
+        VKErrorCode.ADS_ERROR
+    }
+
+    def classify_error(self, code: int, message: int):
+        if code in self.NON_CRITICAL or code in self.RETRYABLE:
+            return ErrorSeverity.NON_CRITICAL
+        elif code in self.CRITICAL:
+            logger.critical(f"critical error code: {code}, msg: {message}")
+            return ErrorSeverity.CRITICAL
+        else:
+            logger.critical(f"unknown error code: {code}, msg: {message}")
+            return ErrorSeverity.UNKNOWN
     
-    def execute(self, func: Callable, *args, **kwargs):
-        attempt = 1
-
-        while attempt <= self.max_retries:
-            try:
-                result =  func(*args, **kwargs)
-                error: ErrorHandlerResult = self.vk_error_handler.handle(result)
-
-                return result
-
-            except self.retry_exceptions as e:
-                logger.error(f"retry_exception error: {e}")
-                attempt += 1
-            except Exception as e:
-                logger.exception(f"Unknown error: {e}")
-                raise e
-        
+    def is_retryable(self, code: int) -> bool:
+        if code in self.RETRYABLE:
+            return True
+        return False
 
 
-class RequestTracker:
+class ResponseParser(ABC):
+    @abstractclassmethod
+    def parse_response(self, response: requests.Response) -> APIResponse:
+        pass
+
+class JSONResponseParser(ResponseParser):
+    def __init__(self, error_classifier: ErrorClassifier):
+        self.error_classifier = error_classifier
+
+    def parse_response(self, response: requests.Response) -> APIResponse:
+        response_json = response.json()
+
+        if "error" in response_json.keys():
+            error = response_json["error"]
+            code = error["error_code"]
+            message = error["error_msg"]
+
+            severity = self.error_classifier.classify_error(code, message)
+            is_retryable = self.error_classifier.is_retryable(code)
+
+            return APIResponse(
+                success=False,
+                data=None,
+                error=APIError(
+                    code=code,
+                    message=message,
+                    severity=severity,
+                    is_retryable=is_retryable
+                )
+            )
+        else:
+            return APIResponse(
+                success=True,
+                data=response_json,
+                error=None
+            )
+
+class RequestTracker(ABC):
+    @abstractclassmethod
+    def acquire(self) -> None:
+        pass
+
+class SynchronousRequestTracker(RequestTracker):
     """
     Апи имеют ограничения на количество запросов в секунду (Request Per Second).
     Данный класс отслеживает RPS и ждет, исчерпан лимит запросов за последнюю 
     секунду
     """
 
-    request_times = deque()
-    exceptedRPS = 5
+    def __init__(self, exceptedRPS: int = 5):
+        self.request_times = deque()
 
-    def __init__(self):
-        self.rp = RetryPolicy()
+        self.exceptedRPS = exceptedRPS
 
-    def send_request(self, *args, **kwargs):
-        """Исполняет запрос только после проверки _check_rps_and_wait_if_need"""
-        self._check_rps_and_wait_if_need()
-
-        def _request() -> dict:
-            response = requests.get(*args, **kwargs)
-            response.raise_for_status()
-            return response.json()
-        
-        self.rp.execute(_request)
-
-    def _check_rps_and_wait_if_need(self) -> None:
+    def acquire(self) -> None:
         """
         Проверяет время запросов в request_times и если количество запросов было 
         равное значению exceptedRPS и все они были менее секунды назад, то 
@@ -87,48 +225,208 @@ class RequestTracker:
                 break
             else:
                 wait_time = self.request_times[0] + 1 - now
-                print(wait_time)
                 time.sleep(wait_time)
 
+class RetryStrategy(ABC):
+    @abstractclassmethod
+    def should_retry(self, attempt, response_error: APIError) -> bool:
+        pass
 
-rt = RequestTracker()
+    @abstractclassmethod
+    def get_delay(self, attempt) -> float:
+        pass
+
+class ExponentialBackoffRetryStrategy(RetryStrategy):
+    def __init__(self, max_retries=3):
+        self.base_delay = 1  # задержка в 1 секунду
+        self.max_retries = max_retries
+    
+    def should_retry(self, attempt: int, response_error: APIError) -> bool:
+        if attempt >= self.max_retries:
+            return False
+        return response_error.is_retryable
+    
+    def get_delay(self, attempt: int) -> float:
+        return self.base_delay * (2 ** attempt)
 
 
-def check_vk_api_error(response_json: dict) -> Optional[VKAPIError]:
-    try:
-        handle_vk_api_error(response_json)
-    except VKAPIError as e:
-        logger.error(f"VKAPIError: {e}")
-        return e
+class APIClient():
+    def __init__(self,
+        error_classifier: ErrorClassifier,
+        response_parser: ResponseParser,
+        request_tracker: RequestTracker,
+        retry_strategy: RetryStrategy
+    ):
+        self.error_classifier = error_classifier
+        self.response_parser = response_parser
+        self.request_tracker = request_tracker
+        self.retry_strategy = retry_strategy
 
-def get_friends(vk_id: int) -> FriendsResponse | VKAPIError:
-    """Вызывает ручку апи получения друзей пользователя vk_id"""
-    response = rt.send_request(
-        "https://api.vk.com/method/friends.get", 
-        params={"user_id": vk_id,
-            "access_token": API_KEY,
-            "v": 5.154,
+        self.request_exceptions = (
+            ConnectionError,
+            Timeout
+        )
+    
+    def make_request(self, 
+        method: HTTPMethods,
+        url: str,
+        params: dict[str, Union[str, int, list[str]]]
+    ) -> APIResponse:
+        attempt = 0
+        
+        while True:
+            try:
+                self.request_tracker.acquire()
+
+                response = self._execute_request(method, url, params)
+
+                parsed_response = self.response_parser.parse_response(response)
+
+                if not(parsed_response.error):
+                    return parsed_response
+                
+                if self.retry_strategy.should_retry(attempt, parsed_response):
+                    time.sleep(self.retry_strategy.get_delay(attempt))
+                else:
+                    logger.error(f"not retryable error: {url} {params}")
+            except self.request_exceptions:
+                network_error = APIResponse(
+                    success=False,
+                    data=None,
+                    error=APIError(
+                        code=0,
+                        message="Netwotk Error",
+                        severity=ErrorSeverity.CRITICAL,
+                        is_retryable=True
+                    )
+                )
+
+                if self.retry_strategy.should_retry(attempt, parsed_response):
+                    time.sleep(self.retry_strategy.get_delay(attempt))
+                else:
+                    logger.error(f"not retryable error: {url} {params}")
+                attempt += 1
+            except Exception as e:
+                logger.exception(e)
+                unknown_error = APIResponse(
+                    success=False,
+                    data=None,
+                    error=APIError(
+                        code=0,
+                        message="Unknown Error",
+                        severity=ErrorSeverity.CRITICAL,
+                        is_retryable=False
+                    )
+                )
+                return unknown_error
+
+    def _execute_request(self,
+        method: HTTPMethods,
+        url: str,
+        params: dict[str, Union[str, int, list[str]]]
+    ) -> requests.Response:
+        match method:
+            case HTTPMethods.GET:
+                return requests.get(url, params=params)
+            case HTTPMethods.POST:
+                return requests.post(url, params=params)
+            case _:
+                ValueError(f"Неизвестный тип запроса: {method}")
+
+class APIResponseToPydanticConverter:
+    def friends(self, response: APIResponse) -> FriendsModel:
+        empty_model = FriendsModel(count=0, items=[])
+        
+        if response.data:
+            response_data = response.data
+            payload = response_data["response"]
+
+            try:
+                return FriendsModel(**payload)
+            except ValueError as e:
+                # Pydantic ValidationError наследуется от ValueError
+                logger.error(f"Pydantic Validation Error: {e}")
+                return empty_model
+        return empty_model
+
+    def users(self, response: APIResponse) -> list[UserModel]:
+        empty_response = []
+        
+        if response.data:
+            response_data = response.data
+            payload = response_data["response"]
+
+            try:
+                return [UserModel(**item) for item in payload]
+            except ValueError as e:
+                # Pydantic ValidationError наследуется от ValueError
+                logger.error(f"Pydantic Validation Error: {e}")
+                return empty_response
+        return empty_response
+
+class VKAPIClient(APIClient):
+    def __init__(self):
+        error_classifier = VKAPIErrorClassifier()
+        response_parser = JSONResponseParser(error_classifier)
+        request_tracker = SynchronousRequestTracker()
+        retry_strategy = ExponentialBackoffRetryStrategy()
+
+        super().__init__(
+            error_classifier,
+            response_parser,
+            request_tracker,
+            retry_strategy
+        )
+
+        self.base_url = "https://api.vk.com/method"
+
+    def request(self, 
+        method, 
+        resource: VKAPIResource, 
+        params: dict[str, Union[str, int, list[str]]]
+    ) -> APIResponse:
+        params.update(
+            {
+                "access_token": API_KEY,
+                "v": VK_API_VERSION
+            }
+        )
+
+        url = f"{self.base_url}/{resource.value}"
+
+        return self.make_request(
+            method,
+            url,
+            params
+        )
+
+class VKAPIInterface:
+    def __init__(self, client: VKAPIClient):
+        self.client = client
+        self.converter = APIResponseToPydanticConverter()
+
+    def get_friends(self, vk_id: int) -> FriendsModel:
+        params = {
+            "user_id": vk_id,
             "order": "hints",
             "count": 1000
         }
-    )
 
-    vk_error = check_vk_api_error(response)
-    if vk_error:
-        return FriendsResponse(**ERROR_RESPONSE)
+        response = self.client.request(
+            HTTPMethods.GET,
+            VKAPIResource.FRIENDS,
+            params
+        )
 
-    # Если vk_error пустой - в ответе точно есть "response"
-    response_body_json = response_json["response"]
+        pydantic_model = self.converter.friends(response)
 
-    logger.info(f"{vk_id} {response_body_json}")
-    response_model = FriendsResponse(**response_body_json)
+        return pydantic_model
 
-    return response_model
-
-def get_user(vk_id: int) -> list[UserInfo] | VKAPIError:
-    response = rt.send_request(
-        "https://api.vk.com/method/users.get", 
-        params={"user_ids": vk_id,
+    def get_users(self, vk_ids: int | list[int]) -> list[UserModel]:
+        if isinstance(vk_ids, int):
+            vk_ids = [vk_ids]
+        params={
+            "user_ids": vk_ids,
             "access_token": API_KEY,
             "v": VK_API_VERSION,
             "fields": [
@@ -147,65 +445,57 @@ def get_user(vk_id: int) -> list[UserInfo] | VKAPIError:
                 "tv","universities","is_verified"
             ]
         }
-    )
 
-    response_json = json.loads(response.text)
+        response = self.client.request(
+            HTTPMethods.GET,
+            VKAPIResource.USERS,
+            params
+        )
 
-    vk_error = check_vk_api_error(response_json)
-    if vk_error:
-        return vk_error
+        pydantic_model = self.converter.users(response)
 
-    # Если vk_error пустой - в ответе точно есть "response"
-    response_body_json = response_json["response"]
+        return pydantic_model
 
-    logger.info(f"{vk_id} {response_body_json}")
-    response_model_list = [UserInfo(**response_user_info) for response_user_info in response_body_json]
-
-    return response_model_list
-
-# @catch_network_errors
-# def get_many_friends(vk_ids: list[int]):
-#     code = f"""
-#     var my_users = {parted_users_list};
-#     var return_data = [];
-#     var a = 0;
-#     while (a != my_users.length) {{
-#         var friends = API.friends.get({{ "user_id":my_users[a] }});
-#         var owner_friends_dict = {{"owner": my_users[a], "friends": friends.items}};
-#         return_data.push(owner_friends_dict);
-
-#         a = a + 1;
-#     }}; 
-#     return return_data;
-#     """
-
-#     friends = requests.get("https://api.vk.com/method/execute",
-#         params = {
-#             "code": code,
-#             "access_token": token,
-#             "v": VK_API_VERSION
-#         }
-#     )
-#     friends = json.loads(friends.text)
-#     return friends['response']
-
-# @bucket_queue
-# def get_users_info(user_ids: list):
-#     code = f"""
-#     var user_ids = {user_ids};
-
-#     var users = API.users.get({{ "user_ids":user_ids }});
+class VKAPICodeInterface:
+    def __init__(self, client: VKAPIClient):
+        self.client = client
+        self.converter = APIResponseToPydanticConverter()
     
-#     return users;
-#     """
+    def _execute(self, code):
+        params = {
+            "code": code
+        }
 
-#     users = requests.get("https://api.vk.com/method/execute",
-#         params = {
-#             "code": code,
-#             "access_token": token,
-#             "v": VK_API_VERSION
-#         }
-#     )
-#     users = json.loads(users.text)
-    
-#     return users['response']
+        return self.client.request(
+            HTTPMethods.GET,
+            VKAPIResource.EXECUTE,
+            params
+        )
+
+    def get_friends(self, vk_ids: list[int]):
+        code = f"""
+        var my_users = {vk_ids};
+        var return_data = [];
+        var a = 0;
+        while (a != my_users.length) {{
+            var friends = API.friends.get({{ "user_id":my_users[a] }});
+            var owner_friends_dict = {{"owner": my_users[a], "friends": friends.items}};
+            return_data.push(owner_friends_dict);
+
+            a = a + 1;
+        }}; 
+        return return_data;
+        """
+
+        return self._execute(code)
+
+    def get_users(self, vk_id: int | list[int]):
+        code = f"""
+        var user_ids = {vk_id};
+
+        var users = API.users.get({{ "user_ids":user_ids }});
+
+        return users;
+        """
+
+        return self._execute(code)
