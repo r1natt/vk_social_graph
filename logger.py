edc0f6e7255ba7f3da5ad1d9998ca242f0d4e43a
lewis <rinatt116@gmail.com>
2025-07-23 00:01:13 +0300

    Another approach to getting a logger
    Call function which config logger in main.py and
    use getLogger function in different part of programm

--- logger.py
+++ logger.py
@@ -1,29 +1,61 @@
 import logging
+import os
 
+LOG_DIR = "./logs"
 
 STREAM_LOG_LEVEL = logging.INFO
 FILE_LOG_LEVEL = logging.DEBUG
+ERROR_FILE_LOG_LEVEL = logging.ERROR
 
-LOGFORMAT = "[%(levelname)s] %(asctime)s %(name)s %(filename)s:%(lineno)s %(funcName)s() - %(message)s"
-logger_formatter = logging.Formatter(LOGFORMAT)
+LOGFORMAT = "%(asctime)s [%(levelname)s] %(name)s:%(funcName)s():%(lineno)s - %(message)s"
+formatter = logging.Formatter(LOGFORMAT)
 
-streamh = logging.StreamHandler()
-streamh.setLevel(STREAM_LOG_LEVEL)
-streamh.setFormatter(logger_formatter)
+def setup_logger():
+    os.makedirs("./logs", exist_ok=True)
 
-fileh = logging.FileHandler("./logs/general.log")
-fileh.setLevel(FILE_LOG_LEVEL)
-fileh.setFormatter(logger_formatter)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
-reqs_file = logging.FileHandler("./logs/reqs.log")
-reqs_file.setLevel(FILE_LOG_LEVEL)
-reqs_file.setFormatter(logger_formatter)
+    # Очистим старые обработчики
+    if logger.hasHandlers():
+        logger.handlers.clear()
 
-general_log = logging.getLogger("general")
-general_log.setLevel(logging.DEBUG)
-general_log.addHandler(fileh)
-general_log.addHandler(streamh)  
+    # Консоль
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(STREAM_LOG_LEVEL)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
 
-reqs_log = logging.getLogger("reqs")
-reqs_log.setLevel(logging.DEBUG)
-reqs_log.addHandler(reqs_file)
+    # Файл debug.log
+    debug_handler = logging.FileHandler("./logs/debug.log")
+    debug_handler.setLevel(FILE_LOG_LEVEL)
+    debug_handler.setFormatter(formatter)
+    logger.addHandler(debug_handler)
+
+    # Файл error.log
+    error_handler = logging.FileHandler("./logs/error.log")
+    error_handler.setLevel(ERROR_FILE_LOG_LEVEL)
+    error_handler.setFormatter(formatter)
+    logger.addHandler(error_handler)
+
+def create_logger(name, logfile, level=logging.DEBUG):
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.propagate = False
+
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    file_handler = logging.FileHandler(os.path.join(LOG_DIR, logfile))
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(level)
+    logger.addHandler(file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(level)
+    logger.addHandler(stream_handler)
+
+    return logger
